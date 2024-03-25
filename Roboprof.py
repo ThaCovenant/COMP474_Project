ae8707f5d3fb17ac458c02f8373dcd93cda9c729
Amish <amish920w@gmail.com>
2024-03-24 20:28:29 -0400

    schema fix
    double course syntax
    add comments

--- Roboprof.py
+++ Roboprof.py
@@ -8,7 +8,6 @@ from AutomatedKnowledgeBaseConstruction import (studentDataToRDFTriples,
 file_path1 = "RDFs/Courses.ttl"
 file_path2 = "RDFs/Lectures.ttl"
 file_path3 = "RDFs/Student_Schema.ttl"
-file_path4 = "RDFs/Topics.ttl"
 file_path5 = "RDFs/University_Schema.ttl"
 file_path6 = "RDFs/Merged.ttl"
 file_path_students = "data/students_grades.csv"
