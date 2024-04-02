aca1787e3a9fbd22e4e24545725a9e908f7b4e69
Amish <amish920w@gmail.com>
2024-04-01 23:18:40 -0400

    courses added from csv
    work needed for vacab use

--- Roboprof.py
+++ Roboprof.py
@@ -10,6 +10,7 @@ file_path2 = "RDFs/Lectures.ttl"
 file_path3 = "RDFs/Student_Schema.ttl"
 file_path5 = "RDFs/University_Schema.ttl"
 file_path6 = "RDFs/Merged.ttl"
+
 file_path_students = "data/students_grades.csv"
 file_path_merge = "Triples/MergedTriples.ttl"
 file_path_students_KB = "Triples/students.ttl"
@@ -18,6 +19,7 @@ file_path_topics = "Triples/topics.ttl"
 file_path_courseMaterial_Comp335 = "data/courseMaterial/COMP335"
 file_path_courseMaterial_Comp474 = "data/courseMaterial/COMP474"
 file_path_lecture = "Triples/lectures.ttl"
+file_path_courses = "Triples/courses.ttl"
 
 
 def main():
@@ -38,8 +40,9 @@ def main():
     # g4.parse(file_path_topics, format="ttl")
 
     g5 = extractCoursesData()
+    createGraph(g5, file_path_courses)
 
-    g6 = g1 + g2 + g3
+    g6 = g1 + g2 + g3 + g5
     # print(g5.serialize(format='turtle'))
     createGraph(g6, file_path_merge)
 
